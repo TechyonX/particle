e9077cc5a2429b027cd71697e523d08b9883308a
Bilguun Ochirbat <bilguun0203@gmail.com>
2023-04-12 22:03:03 +0800

    feat: middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,11 +2,19 @@ import { createMiddlewareSupabaseClient } from "@supabase/auth-helpers-nextjs";
 import { NextResponse } from "next/server";
 
 import type { NextRequest } from "next/server";
-import { Database } from "./database.types";
+import { Database } from "./lib/database.types";
 
 export async function middleware(req: NextRequest) {
   const res = NextResponse.next();
   const supabase = createMiddlewareSupabaseClient<Database>({ req, res });
-  await supabase.auth.getSession();
+  const {
+    data: { session },
+  } = await supabase.auth.getSession();
+
+  console.log("session", session);
+  if (session && req.nextUrl.pathname.startsWith("/login")) {
+    return NextResponse.redirect(new URL("/", req.url));
+  }
+
   return res;
 }
