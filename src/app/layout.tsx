5eb4faaf5ad0fc4b5e87f4bfe1b1f8f221a58a78
khasbilegt <khasbilegt.ts@gmail.com>
2023-04-16 05:15:46 +0800

    update home

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,6 +27,11 @@ export default function RootLayout({
         theme ? theme.value : "dark"
       } bg-gray-50 dark:bg-gray-900`}
     >
+      <title>Particle - the ultimate digital content manager</title>
+      <meta
+        name="description"
+        content="Particle - the ultimate digital content manager. With Particle, you can easily organize and access all your digital content in one place. Our AI-powered search features allow you to quickly find what you need, while our natural language processing technology helps you better manage and categorize your content."
+      />
       <body className="min-h-full bg-gradient-to-b from-gray-50 to-gray-100 dark:from-gray-900 dark:to-gray-950">
         <SupabaseProvider>{children}</SupabaseProvider>
       </body>
